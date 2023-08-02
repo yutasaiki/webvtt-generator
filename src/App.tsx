ee1271e5e98927533eaee9fcfdcd234fe04e1900
yuta-saiki <y.saiki@cocolofun.co.jp>
2023-08-02 22:21:52 +0900

    字幕リストの要素の順番をドラッグ＆ドロップで入れ替えられるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { css } from '../styled-system/css';
 import { ChangeEventHandler, useCallback, useEffect, useRef } from 'react'
+import update from "immutability-helper";
 import { useSetState } from 'react-use';
-import { TrackItem } from './components/TrackItem/TrackItem';
 import { convertWebVtt } from './utils/convertWebVtt';
+import { TrackListItem } from './components/TrackListItem/TrackListItem';
 
 type State = {
   /**
@@ -122,7 +123,7 @@ function App() {
       return
     }
     setupTextTrack(video)
-    
+
     video.addEventListener("durationchange", () => {
       setState({ duration: Math.floor(video.duration) })
     })
@@ -133,34 +134,41 @@ function App() {
       e.returnValue = "編集をやめてページを離れますか？"; // Google Chromeのみ機能する
     })
   }, [setState, setupTextTrack, state.videoTextTrack, state.vttCues.length])
+
+  const handleSortEnd = useCallback((dragIndex: number, hoverIndex: number) => {
+    setState(({ vttCues })=>({
+      vttCues: update(vttCues, {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, vttCues[dragIndex] as VTTCue]
+        ]
+      })
+    }))
+  }, [setState])
   return (
-    <div className={css({display: "flex", flexDirection: "column", margin: "0 auto" })}>
+    <div className={css({ display: "flex", flexDirection: "column", margin: "0 auto" })}>
       <h1 className={css({ margin: "8px 0", fontSize: "2rem", fontWeight: "bold" })}>WebVTTジェネレーター </h1>
       <p className={css({ display: "flex", justifyContent: "center", textAlign: "left", fontSize: "12px", margin: "12px 0" })}>このツールは動画で字幕表示を確認しながらVTT形式のファイルを作成できるツールです。使い方はとても簡単で右側のペインで字幕作成したい動画を読み込ませた後、左側のペインで字幕を追加していきます。<br />「プレビュー」ボタンをクリックすることで動画へ作成した字幕を反映できます。また動画下にVTT形式でファイルの中身が表示され「.vttをダウンロード」ボタンをクリックすることでVTT形式のファイルがお手元にダウンロードされます。</p>
       <div className={css({ display: "flex", justifyContent: "center", gap: "16px" })}>
-      <div className={css({ flexGrow: 1, overflowY: "scroll", border: "1px solid #222222", maxHeight: "620px" })} >
-        <div className={css({display: "flex", gap: "16px", justifyContent: "center", margin: "16px 0"})}>
-          <button className={css({ backgroundColor: "#007bff", color: "#ffffff", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "4px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" }})} onClick={addTrack} >追加</button>
-          <button className={css({ backgroundColor: "#ffc107", color: "#222222", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "4px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" } })} onClick={generateTrackText}>プレビュー</button>
+        <div className={css({ flexGrow: 1, overflowY: "scroll", border: "1px solid #222222", maxHeight: "620px" })} >
+          <div className={css({ display: "flex", gap: "16px", justifyContent: "center", margin: "16px 0" })}>
+            <button className={css({ backgroundColor: "#007bff", color: "#ffffff", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "4px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" } })} onClick={addTrack} >追加</button>
+            <button className={css({ backgroundColor: "#ffc107", color: "#222222", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "4px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" } })} onClick={generateTrackText}>プレビュー</button>
+          </div>
+          <ul className={css({ padding: 0 })}>
+            {state.vttCues.length !== 0 ? state.vttCues.map((cue, index) => {
+              return (<TrackListItem index={index} cue={cue} duration={state.duration} onSortEnd={handleSortEnd} onDelete={() => deleteTrack(cue.id)} onChangeTrackText={(e) => handleChangeTrackText(index, e.currentTarget.value)} onChangeStartTime={(e) => handleChangeStartTime(index, Number.parseInt(e.currentTarget.value))} onChangeEndTime={(e) => handleChangeEndTime(index, Number.parseInt(e.currentTarget.value))} />)
+            }) : <p>動画を読み見込ませてテキストトラックを追加しましょう！</p>}
+          </ul>
         </div>
-        <ul className={css({ padding: 0 })}>
-          {state.vttCues.length !== 0 ? state.vttCues.map((cue, index) => {
-            return (<li className={css({ padding: "4px 8px", listStyleType: "none", gap: "8px", display: "flex", justifyContent: "center" })} key={cue.id}>
-              <span className={css({ fontSize: "18px", fontWeight: "bold" })}>{index + 1}</span>
-              <TrackItem cue={cue} duration={state.duration} onChangeTrackText={(e) => handleChangeTrackText(index, e.currentTarget.value)} onChangeStartTime={(e) => handleChangeStartTime(index, Number.parseInt(e.currentTarget.value))} onChangeEndTime={(e) => handleChangeEndTime(index, Number.parseInt(e.currentTarget.value))} ></TrackItem>
-              <button className={css({ backgroundColor: "#dc3545", color: "#ffffff", width: "72px", fontSize: "12px", fontWeight: "bold", padding: "4px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" }})} onClick={() => deleteTrack(cue.id)}>削除</button>
-            </li>)
-          }) : <p>動画を読み見込ませてテキストトラックを追加しましょう！</p>}
-        </ul>
-      </div>
-      <div className={css({ flexGrow: 1, display: "flex", flexDirection: "column", gap: 2 })}>
-        <input type="file" id="movieFile" accept='.mp4' onChange={handleChangeMovieFile} />
-        <video ref={videoRef} controls className={css({width: "100%"})}/>
-        <textarea className={css({textAlign: "left", border: "solid 1px #222222", borderRadius: "4px", height: "300px", resize: "none", width: "100%"})} rows={10} value={state.vttText} />
-        <div className={css({ display: "flex", justifyContent: "center" })}>
-        <button className={css({ backgroundColor: "#ffc107", color: "#222222", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "8px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" }})} onClick={downloadVttFile}>.vttをダウンロード</button>
+        <div className={css({ flexGrow: 1, display: "flex", flexDirection: "column", gap: 2 })}>
+          <input type="file" id="movieFile" accept='.mp4' onChange={handleChangeMovieFile} />
+          <video ref={videoRef} controls className={css({ width: "100%" })} />
+          <textarea className={css({ textAlign: "left", border: "solid 1px #222222", borderRadius: "4px", height: "300px", resize: "none", width: "100%" })} rows={10} value={state.vttText} />
+          <div className={css({ display: "flex", justifyContent: "center" })}>
+            <button className={css({ backgroundColor: "#ffc107", color: "#222222", width: "200px", fontSize: "12px", fontWeight: "bold", padding: "8px 0", borderRadius: "4px", _hover: { opacity: "0.7", cursor: "pointer" } })} onClick={downloadVttFile}>.vttをダウンロード</button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
--- src/components/Track/Track.tsx
+++ src/components/Track/Track.tsx
@@ -1,19 +1,21 @@
 import { css } from '../../../styled-system/css';
 import { ChangeEventHandler } from "react"
 
-export function TrackItem({
+export type TrackProps = {
+    cue: VTTCue;
+    duration: number;
+    onChangeTrackText: ChangeEventHandler<HTMLInputElement>;
+    onChangeStartTime: ChangeEventHandler<HTMLInputElement>;
+    onChangeEndTime: ChangeEventHandler<HTMLInputElement>;
+}
+
+export function Track({
     cue,
     duration,
     onChangeTrackText,
     onChangeStartTime,
     onChangeEndTime
-}: {
-    cue: VTTCue
-    duration: number
-    onChangeTrackText: ChangeEventHandler<HTMLInputElement>
-    onChangeStartTime: ChangeEventHandler<HTMLInputElement>
-    onChangeEndTime: ChangeEventHandler<HTMLInputElement>
-}) {
+}: TrackProps) {
     return (<>
         <input className={css({ border: "solid 1px #222222", borderRadius: "4px" })} type="number" name="startTime" onChange={(e) => onChangeStartTime(e)} value={cue.startTime} min="0" max={duration} size={1} />
         <input className={css({ border: "solid 1px #222222", borderRadius: "4px" })} type="number" name="endTime" onChange={(e) => onChangeEndTime(e)} value={cue.endTime} min="0" max={duration} size={1} />
